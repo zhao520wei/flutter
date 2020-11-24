9a53a7d79adbeb422b8146fc6b139106e0740194
zhao520wei <zhao520wei1@163.com>
2020-11-24 10:21:22 +0800

    plugin init

---
+++ .android/app/src/main/java/zhaowei/com/example/fluttermodule/host/MainActivity.java
@@ -0,0 +1,7 @@
+package zhaowei.com.example.fluttermodule.host;
+
+import android.os.Bundle;
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
